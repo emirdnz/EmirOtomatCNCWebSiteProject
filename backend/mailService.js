e6027567fec838937d89042b103290ef9288f145
emirdnz <emirdeniz997@gmail.com>
2025-05-09 10:59:20 +0300

    cahged mailservcice.js port number

--- backend/mailService.js
+++ backend/mailService.js
@@ -247,7 +247,7 @@ app.post('/send-mail', upload.array('files', 5), async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT|| 5000;
 app.listen(PORT, () => {
   console.log(`Server ${PORT} portunda çalışıyor`);
 });
